fd5a03c5319cb242f3b16875341ace5b56b35dcd
AnnKrott <krotova_anastasiya@inbox.ru>
2023-09-21 01:54:54 +0300

    added timer and modal window

--- 2__Food/dist/js/main.js
+++ 2__Food/dist/js/main.js
@@ -36,3 +36,95 @@ window.addEventListener('DOMContentLoaded', () => {
     hideTabs();
     showTheTab();
 })
+
+// timer
+
+const deadline = '2024-05-16';
+
+function getTimeRemainig(endtime) {
+    const difference = Date.parse(endtime) - Date.parse(new Date());
+
+    const sec = difference / 1000 % 60;
+    const min = Math.floor(difference / (1000 * 60) % 60);
+    const hours = Math.floor(difference / (1000 * 60 * 60) % 24);
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    return {
+        'difference': difference,
+        'sec': sec,
+        'min': min,
+        'hours': hours,
+        'days': days,
+    }
+}
+
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return num = `0${num}`
+    } else {
+        return num;
+    }
+}
+
+function setTimer(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const sec = timer.querySelector('#seconds');
+    const min = timer.querySelector('#minutes');
+    const hours = timer.querySelector('#hours');
+    const days = timer.querySelector('#days');
+    const timerID = setInterval(updateTimer, 1000);
+    updateTimer();
+
+    function updateTimer() {
+        const remainingTime = getTimeRemainig(endtime);
+        difference = getZero(remainingTime.difference);
+        sec.innerHTML = getZero(remainingTime.sec);
+        min.innerHTML = getZero(remainingTime.min);
+        hours.innerHTML = getZero(remainingTime.hours);
+        days.innerHTML = getZero(remainingTime.days);
+
+        if (remainingTime <= 0) {
+            clearInterval(timerID);
+        }
+    }
+}
+setTimer('.timer', deadline);
+
+//modal
+
+const openModalBtn = document.querySelectorAll('[data-modal]'),
+    closeModalBtn = document.querySelector('[data-close'),
+    modal = document.querySelector('.modal');
+
+
+openModalBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+    })
+})
+
+function closeModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', e => {
+    if (e.target === modal) {
+        closeModal();
+    }
+})
+
+document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+    }
+})
+
+
+
+
+
+
